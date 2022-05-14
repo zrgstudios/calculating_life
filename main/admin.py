74361c4c4cf355c674698269595798f040aae2ea
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2022-05-13 22:14:25 -0400

    Video 9: Foreign Keys
    Connected our tables. Only temporary...

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 from tinymce.widgets import TinyMCE
 from django.db import models
 
@@ -12,6 +12,8 @@ class TutorialAdmin(admin.ModelAdmin):
 
     fieldsets = [
         ("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
+        ("URL", {"fields": ["tutorial_slug"]}),
+        ("Series", {"fields": ["tutorial_series"]}),
         ("Content", {"fields": ["tutorial_content"]})
     ]
 
@@ -20,4 +22,6 @@ class TutorialAdmin(admin.ModelAdmin):
     }
 
 
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
 admin.site.register(Tutorial, TutorialAdmin)
